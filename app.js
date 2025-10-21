1f14f4a9734788c939b8e59d2d1a67d3b8ab89ce
Dushyant Chauhan <dushyantr737@gmail.com>
2025-10-21 22:51:51 +0530

    Refactor event listeners and add background color change
    Removed unused button event listeners and added background color change functionality on parent click.

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const button = document.querySelector("#btn");
-const text = document.querySelector("#text1");
+// const red = document.querySelector('#red')
+// const blue = document.querySelector('#blue')
+// const yellow = document.querySelector('#yellow')
+// const black = document.querySelector('#black')
 
-const number = ["Hello Boys", "Hello Girls", "Hello Youtubers", "Hello instagram", "Hello twitter", "Hello coders", "Hello child", "Hello whatsapp", "Hello gyan gaming", "Hello gamers", "Hello sporters"];
 
-button.addEventListener("click", () => {
-    const random = Math.floor(Math.random() * number.length);
-    text.textContent=number[random]
-});
+// const orange = document.querySelector('#orange')
+const parent=document.querySelector('#parent')
+//good syantx
+parent.addEventListener('click', (e) => {
+    const value = e.target.id;
+    document.body.style.backgroundColor= value;
+})
+
+// Bad syntax
+// red.addEventListener('click', () => {
+//     document.body.style.backgroundColor = "red";
+//     console.log("clicked")
+// })
+
+
+// blue.addEventListener('click', () => {
+//     document.body.style.backgroundColor = "blue";
+//     console.log("clicked")
+// })
+
+
+// yellow.addEventListener('click', () => {
+//     document.body.style.backgroundColor = "yellow";
+//     console.log("clicked")
+
+// })
+
+
+// black.addEventListener('click', () => {
+//     document.body.style.backgroundColor = "black";
+//     console.log("clicked")
+// })
+
+// orange.addEventListener('click', () => {
+//     document.body.style.backgroundColor = "orange";
+//     console.log("clicked")
+// })
